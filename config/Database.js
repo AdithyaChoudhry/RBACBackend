699c87440b565d85e698c29eb1c6f1f44170538c
Adithya000 <86005781+Adithya000@users.noreply.github.com>
2024-11-27 07:48:16 +0530

    'addurl2'

--- config/Database.js
+++ config/Database.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Use DATABASE_URL from Railway, or a fallback for local development
-const DATABASE_URL = process.env.DATABASE_URL || 'mysql://root:@127.0.0.1:3311/rbac'; // Fallback for local testing
+const DATABASE_URL = process.env.DATABASE_URL
 
 const db = new Sequelize(DATABASE_URL, {
     dialect: "mysql",
