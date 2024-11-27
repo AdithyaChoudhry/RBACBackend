7509ea06c923653060b82e80237c724a65624d38
Adithya000 <86005781+Adithya000@users.noreply.github.com>
2024-11-27 14:44:18 +0530

    'h'

--- config/Database.js
+++ config/Database.js
@@ -1,16 +1,18 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQLUSER, process.env.MYSQL_ROOT_PASSWORD, {
+const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQLUSER, process.env.MYSQL_ROOT_PASSWORD, {
     host: process.env.MYSQLHOST,
     port: process.env.MYSQLPORT,
     dialect: 'mysql',
 });
 
 // Authenticate
-sequelize.authenticate()
+db .authenticate()
     .then(() => {
         console.log('Connection has been established successfully.');
     })
     .catch(err => {
         console.error('Unable to connect to the database:', err);
     });
+
+export default db;
