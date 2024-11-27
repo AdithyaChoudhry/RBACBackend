8c36fc4b21023c7557eccea84f24fb21aa0fd7a1
Adithya000 <86005781+Adithya000@users.noreply.github.com>
2024-11-27 15:01:52 +0530

    'h'

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize('railway', 'root', JOOkDLKlkONETXbvhHSFbBQQkMsNxTSa, {
+const db = new Sequelize('railway', 'root','JOOkDLKlkONETXbvhHSFbBQQkMsNxTSa', {
   host: "mysql.railway.internal",
   port: 33765, // Ensure this matches your MySQL configuration
   dialect: "mysql"
