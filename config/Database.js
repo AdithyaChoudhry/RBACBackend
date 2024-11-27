55e5cf70eb730bc1a8477b4ff8f424e7b1de948c
Adithya000 <86005781+Adithya000@users.noreply.github.com>
2024-11-27 07:21:08 +0530

    'adddb'

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('rbac', 'root', '', {
     host: "127.0.0.1",
-    port: 3312, // Ensure this matches your MySQL configuration
+    port: 3311, // Ensure this matches your MySQL configuration
     dialect: "mysql"
 });
 
