5de3709aecba0784b30c65a212cc6fd66f8a6071
Adithya000 <86005781+Adithya000@users.noreply.github.com>
2024-11-27 13:37:20 +0530

    'url'

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(session({
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: `https://rbacbackend-production.up.railway.app/`
 }));
 app.use(express.json());
 app.use(UserRoute);
